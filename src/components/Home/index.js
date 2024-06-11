b2c2f01e089be9674ae5fa97ef0f76a095a90d60
Viveklagishetty <Viveklagishetty@gmail.com>
2024-06-11 06:00:21 +0000

    fetchAndRouting

---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import UserInfo from '../UserInfo'
+
+import BlogsList from '../BlogList'
+
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <UserInfo />
+    <BlogsList />
+  </div>
+)
+
+export default Home
